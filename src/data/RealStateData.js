68a28e89ad536bcd99248360a0b7893334fb0690
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-04-17 14:46:25 -0300

    feat/RLCR-9: update properties description

--- src/data/RealStateData.js
+++ src/data/RealStateData.js
@@ -25,11 +25,8 @@ const properties = [
     RKCode: '9781',
     title: 'Solar das Bromélias',
     region: 'Ingleses',
-    description: `IMÓVEL COM MATRÍCULA
-    Totalmente mobiliada com muito bom gosto e sofisticação.
-    Situada na área nobre da praia dos Ingleses, a localização é bastante tranquila e próxima ao comércio local, proporcionando assim um ambiente agradável.
-    O imóvel possui uma posição solar privilegiada, leste permitindo uma ótima iluminação e ventilação natural em todos os cômodos do imóvel.
-    O condomínio, por sua vez, oferece uma área de lazer completa, proporcionando opções de entretenimento e relaxamento para você e sua família.`,
+    description:
+      '**IMÓVEL COM MATRÍCULA**\n\n Totalmente mobiliada com muito bom gosto e sofisticação.\n\n Situada na área nobre da praia dos Ingleses, a localização é bastante tranquila e próxima ao comércio loca proporcionando assim um ambiente agradável.\n\n O imóvel possui uma posição solar privilegiada, leste permitindo uma ótima iluminação e ventilação natural todos os cômodos do imóvel.\n\n O condomínio, por sua vez, oferece uma área de lazer completa, proporcionando opções de entretenimento relaxamento para você e sua família.',
     price: 'R$ 565.000,00',
     details: {
       square: '43,10 m²',
@@ -46,24 +43,14 @@ const properties = [
       '/properties/001F-BROMELIAS.webp',
     ],
   },
-
   {
     id: crypto.randomUUID(),
     brokerId: BROKER_ID,
     RKCode: '8870',
     title: 'Res. Miragem',
     region: 'Ingleses',
-    description: `IMÓVEL COM MATRÍCULA
-    Localizado entre o mar e as montanhas, o Residencial Miragem, oferece padrão de qualidade na construção, aliado a uma infraestrutura de lazer que garantirá diversão e conforto para você e sua família.
-    Diferenciais:
-    Captação de água da chuva;
-    Controle de acesso livre de chaves;
-    Infraestrutura para ar condicionado;
-    Áreas de lazer decoradas;
-    Isolamento acústico.
-    Apartamento térreo tipo garden. Face norte.
-    Apartamento de 2 quartos, sendo 1 suíte, uma vaga de garagem com mais de 81 m², com jardim particular gramado com mais de 47m².
-    Aceita financiamento bancário.`,
+    description:
+      '**IMÓVEL COM MATRÍCULA**\n\n Localizado entre o mar e as montanhas, o Residencial Miragem, oferece padrão de qualidade na construção, aliado a uma infraestrutura de lazer que garantirá diversão e conforto para você e sua família.\n\n Diferenciais:\n\n Captação de água da chuva;\n\n Controle de acesso livre de chaves;\n\n Infraestrutura para ar condicionado;\n\n Áreas de lazer decoradas;\n\n Isolamento acústico.\n\n Apartamento térreo tipo garden. Face norte.\n\n Apartamento de 2 quartos, sendo 1 suíte, uma vaga de garagem com mais de 81 m², com jardim particular gramado com mais de 47m².\n\n Aceita financiamento bancário.',
     price: 'R$ 790.000,00',
     details: {
       square: '81,89 m²',
@@ -80,19 +67,14 @@ const properties = [
       '/properties/002f-Miragem.webp',
     ],
   },
-
   {
     id: crypto.randomUUID(),
     brokerId: BROKER_ID,
     RKCode: '9571',
     title: 'Com. Osvaldo Gomez',
     region: 'Ingleses',
-    description: `IMÓVEL COM MATRÍCULA
-    EXCLUSIVIDADE RK IMÓVEIS
-    Apartamento SEMIMOBILIADO com 02 dormitórios, sendo 01 suíte. Banheiro social, sala de estar, cozinha conjugada com sala de jantar, lavanderia, sacada com churrasqueira e pia, vaga de garagem e hobby box.
-    Imóvel frente rua
-    Área privativa de 80,89 m²
-    Residencial localizado na área nobre da Praia Ingleses, cerca de 600m do mar, e bem próximo ao centrinho da praia. O condomínio possui piscina, salão de festas mobiliado e equipado, elevador, interfone e portão eletrônico.`,
+    description:
+      '**IMÓVEL COM MATRÍCULA**\n\n **EXCLUSIVIDADE RK IMÓVEIS**\n\n Apartamento SEMIMOBILIADO com 02 dormitórios, sendo 01 suíte. Banheiro social, sala de estar, cozinha conjugada com sala de jantar, lavanderia, sacada com churrasqueira e pia, vaga de garagem e hobby box.\n\n Imóvel frente rua \n\n Área privativa de 80,89 m²\n\n Residencial localizado na área nobre da Praia Ingleses, cerca de 600m do mar, e bem próximo ao centrinho da praia. O condomínio possui piscina, salão de festas mobiliado e equipado, elevador, interfone e portão eletrônico.',
     price: 'R$ 700.000,00',
     details: {
       square: '80,89 m²',
@@ -110,19 +92,14 @@ const properties = [
       '/properties/003g-Osvaldo.webp',
     ],
   },
-
   {
     id: crypto.randomUUID(),
     brokerId: BROKER_ID,
     RKCode: '8068',
     title: 'Res. Ilha de Citera',
     region: 'Ingleses',
-    description: `IMÓVEL COM MATRÍCULA
-    Incorporação Imobiliária nº R-168.700 do Cartório do 2º Ofício de Registro de Imóveis de Florianópolis – SC.
-    Entrega prevista MARÇO/2024
-    Conheça o mais novo empreendimento na Praia dos Ingleses - RESIDENCIAL ILHA DE CITERA está localizado na entrada da praia - próximo a melhor região da praia. Invista em imóvel na planta!
-    Apartamento NOVO de 03 dormitórios sendo 01 suíte, banheiro social, sala de estar e jantar, cozinha, área de serviço e sacada com churrasqueira a carvão e vaga de garagem.
-    Ótimo acabamento e com excelentes condições de pagamento!`,
+    description:
+      '**IMÓVEL COM MATRÍCULA**\n\n Incorporação Imobiliária nº R-168.700 do Cartório do 2º Ofício de Registro de Imóveis de Florianópolis – SC.\n\n Entrega prevista MARÇO/2024\n\n Conheça o mais novo empreendimento na Praia dos Ingleses - RESIDENCIAL ILHA DE CITERA está localizado na entrada da praia - próximo a melhor região da praia. Invista em imóvel na planta!\n\n Apartamento NOVO de 03 dormitórios sendo 01 suíte, banheiro social, sala de estar e jantar, cozinha, área de serviço e sacada com churrasqueira a carvão e vaga de garagem.\n\n Ótimo acabamento e com excelentes condições de pagamento!',
     price: 'R$ 669.000,00',
     details: {
       square: '81,43 m²',
@@ -140,17 +117,14 @@ const properties = [
       '/properties/004g-Citera.webp',
     ],
   },
-
   {
     id: crypto.randomUUID(),
     brokerId: BROKER_ID,
     RKCode: '9396',
     title: 'Della Giustina',
     region: 'Ingleses',
-    description: `IMÓVEL COM MATRÍCULA
-    Apartamento MOBILIADO e decorado com 03 dormitórios, sendo 01 suíte, banheiro social, ampla sala de estar conjugada com cozinha, área de serviço separada, sacada com churrasqueira, hobbybox e vaga de garagem.
-    IMÓVEL finamente decorado, com móveis planejados em todo apartamento.
-    RESIDENCIAL DELLA GIUSTINA está localizado na área nobre da Praia dos Ingleses, cerca de 700 metros do mar, possui área de lazer com piscina, playground e salão de festas, além de portão eletrônico e interfone com acesso por TAG.`,
+    description:
+      '**IMÓVEL COM MATRÍCULA**\n\n Apartamento MOBILIADO e decorado com 03 dormitórios, sendo 01 suíte, banheiro social, ampla sala de estar conjugada com cozinha, área de serviço separada, sacada com churrasqueira, hobbybox e vaga de garagem.\n\n IMÓVEL finamente decorado, com móveis planejados em todo apartamento.\n\n RESIDENCIAL DELLA GIUSTINA está localizado na área nobre da Praia dos Ingleses, cerca de 700 metros do mar, possui área de lazer com piscina, playground e salão de festas, além de portão eletrônico e interfone com acesso por TAG.',
     price: 'R$ 899.000,00',
     details: {
       square: '87,86 m²',
